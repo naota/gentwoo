54c70cd0389bca4fc50814b03086430fb8cc8944
Naohiro Aota <naota@elisp.net>
2011-05-22 23:50:11 +0900

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gentwoo::Application.config.secret_token = '89ec068dbe0ec5afd2f33e16eada24dcdefcceb5e896aef996c2af39088c19eeda17ea16dc4c103e476653207fa0f692e25c7a84dfc3b7699eff1c8c442bdc61'
