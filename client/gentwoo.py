e37472b02b1931700825ddd224a03cee238995d1
Naohiro Aota <naota@elisp.net>
2011-05-23 20:34:09 +0900

    keyを使うようにした

--- client/gentwoo.py
+++ client/gentwoo.py
@@ -8,12 +8,13 @@ category = "app-shells"
 name = "bash"
 version = "4.2_p8-r1"
 
-data = json.dumps({"emerge": {"user": "naota344",
-                              "duration": 3200,
-                              "buildtime": "2011-05-23T09:10:30Z",
-                              "package":{"category": category,
-                                         "name": name,
-                                         "version": version}}})
+data = json.dumps({"emerge": {"duration": 3200,
+                              "buildtime": "2011-05-23T09:10:30Z"},
+                   "package":{"category": category,
+                              "name": name,
+                              "version": version},
+                   "user": "naota344",
+                   "token": "c50a5090f305b82ab6a5"})
 
 req = urllib2.Request(url, data, {"Content-Type": "application/json",
                                   "Accept": "application/json"})
