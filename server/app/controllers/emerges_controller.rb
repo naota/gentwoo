581fd9ca1eb64fd8433f69baddc877fc6f02b17f
Naohiro Aota <naota@elisp.net>
2011-05-23 18:19:30 +0900

    json で emerge を post できるようにした

--- server/app/controllers/emerges_controller.rb
+++ server/app/controllers/emerges_controller.rb
@@ -1,4 +1,5 @@
 class EmergesController < ApplicationController
+  #before_filter :login_required, :only => [:create, :destroy]
   # GET /emerges
   # GET /emerges.xml
   def index
@@ -23,51 +24,63 @@ class EmergesController < ApplicationController
 
   # GET /emerges/new
   # GET /emerges/new.xml
-  def new
-    @emerge = Emerge.new
+  # def new
+  #   @emerge = Emerge.new
 
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @emerge }
-    end
-  end
+  #   respond_to do |format|
+  #     format.html # new.html.erb
+  #     format.xml  { render :xml => @emerge }
+  #   end
+  # end
 
   # GET /emerges/1/edit
-  def edit
-    @emerge = Emerge.find(params[:id])
+  # def edit
+  #   @emerge = Emerge.find(params[:id])
+  # end
+
+  def getPackage(pkg)
+    @package = Package.where(["category = ? AND name = ? AND version = ?",
+                              pkg[:category], pkg[:name], pkg[:version]]).first
+    @package = Package.new(pkg) unless @package
+    @package
   end
 
   # POST /emerges
   # POST /emerges.xml
   def create
-    @emerge = Emerge.new(params[:emerge])
+    @user = User.find_by_login(params[:emerge][:user]) # current_user
+    @package = getPackage(params[:emerge][:package])
+    params[:emerge][:package] = nil
+    params[:emerge][:user] = nil
+    @emerge = @user.emerge.build(params[:emerge])
+    @emerge.package = @package
 
     respond_to do |format|
       if @emerge.save
-        format.html { redirect_to(@emerge, :notice => 'Emerge was successfully created.') }
         format.xml  { render :xml => @emerge, :status => :created, :location => @emerge }
+        format.json  { render :json => @emerge, :status => :created, :location => @emerge }
       else
-        format.html { render :action => "new" }
         format.xml  { render :xml => @emerge.errors, :status => :unprocessable_entity }
+        format.json  { render :json => @emerge.errors, :status => :unprocessable_entity }
       end
     end
   end
 
   # PUT /emerges/1
   # PUT /emerges/1.xml
-  def update
-    @emerge = Emerge.find(params[:id])
+  # def update
+  #   @emerge = Emerge.find(params[:id])
 
-    respond_to do |format|
-      if @emerge.update_attributes(params[:emerge])
-        format.html { redirect_to(@emerge, :notice => 'Emerge was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @emerge.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
+  #   respond_to do |format|
+  #     if @emerge.update_attributes(params[:emerge])
+  #       format.html { redirect_to(@emerge, :notice => 'Emerge was successfully updated.') }
+  #       format.xml  { head :ok }
+  #     else
+  #       format.html { render :action => "edit" }
+  #       format.xml  { render :xml => @emerge.errors, :status => :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # DELETE /emerges/1
   # DELETE /emerges/1.xml
--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -1,5 +1,3 @@
 class User < TwitterAuth::GenericUser
-  # Extend and define your user model as you see fit.
-  # All of the authentication logic is handled by the 
-  # parent TwitterAuth::GenericUser class.
+  has_many :emerge
 end
