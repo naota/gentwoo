ba19833266dd7d4e04ccb8efb735e467f29ac244
Naohiro Aota <naota@elisp.net>
2011-05-25 06:02:30 +0900

    コメントした時につぶやく

--- server/app/controllers/comments_controller.rb
+++ server/app/controllers/comments_controller.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 class CommentsController < ApplicationController
   before_filter :login_required, :only => [:create]
 
@@ -5,6 +6,30 @@ class CommentsController < ApplicationController
     @emerge = Emerge.find(params[:emerge_id])
     params[:comment][:user_id] = current_user
     @comment = @emerge.comments.create(params[:comment])
+    
+    if current_user.tweet_comment
+      head = "@"+@emerge.user.login+" さんの"+@emerge.fullname+"に"
+      body = @comment.content
+      foot = "コメントしました。 "+"http://gentwoo.elisp.net"+emerge_path(@emerge)
+
+      headlen = head.split(//u).length
+      bodylen = body.split(//u).length
+      footlen = foot.split(//u).length
+      current_user.twitter.post('/statuses/update.json', :status => 
+                                if headlen + bodylen + footlen <= 137
+                                  head + "「" + body + "」と" + foot
+                                elsif headlen + footlen > 140
+                                  "@"+@emerge.user.login+" さんにコメントしました。 "+
+                                    "http://gentwoo.elisp.net"+emerge_path(@emerge)
+                                elsif headlen + footlen > 136
+                                  head + body
+                                else
+                                  head + "「" + 
+                                    body.split(//u)[0,136-headlen-footlen].join('') +
+                                    "…」と" + foot
+                                end )
+    end
+
     redirect_to emerge_path(@emerge)
   end
 end
