edb4992404c415a54e5da1c9d64772f48d4ede24
Naohiro Aota <naota@elisp.net>
2011-05-25 02:59:50 +0900

    パッケージ名も表示するように

--- server/app/models/emerge.rb
+++ server/app/models/emerge.rb
@@ -3,6 +3,9 @@ class Emerge < ActiveRecord::Base
   belongs_to :user
   has_many :comments, :as => :commentable
 
+  def fullname
+    self.package.fullname
+  end
   def page
     "emerges/" + self.id.to_s
   end
