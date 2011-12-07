a6b3e484caf52b89913b819b8e9bebddbd13d3fa
SAKURAI Masashi <m.sakurai@kiwanami.net>
2011-12-07 23:56:22 +0900

    Added cache tables and tuned up the dashboard page.

--- server/app/models/package.rb
+++ server/app/models/package.rb
@@ -1,6 +1,7 @@
 class Package < ActiveRecord::Base
   has_many :emerges
   has_many :comment, :as => :commentable
+  has_one :cache_pop_package
 
   def fullname
     category + "/" + name + "-" + version
