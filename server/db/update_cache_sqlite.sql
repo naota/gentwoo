fd51d00f330e6fdfc31285874bf3ea5e910e346d
SAKURAI Masashi <m.sakurai@kiwanami.net>
2011-12-18 14:43:50 +0900

    modified for mysql

--- server/db/update_cache_sqlite.sql
+++ server/db/update_cache_sqlite.sql
@@ -6,13 +6,13 @@ DELETE FROM cache_pop_users;
 INSERT INTO cache_pop_packages (cnt, package_id, created_at)
   SELECT count(emerges.id) AS cnt, packages.id, CURRENT_TIMESTAMP FROM packages 
   INNER JOIN emerges ON emerges.package_id = packages.id 
-  WHERE (buildtime > date_add(CURRENT_TIMESTAMP, interval -7 day)) 
+  WHERE (buildtime > datetime(CURRENT_TIMESTAMP, '-7 days')) 
   GROUP BY package_id;
 
 INSERT INTO cache_pop_users (cnt, user_id, created_at)
   SELECT count(emerges.id) AS cnt, users.id,CURRENT_TIMESTAMP FROM users
   INNER JOIN emerges ON emerges.user_id = users.id
-  WHERE (buildtime > date_add(CURRENT_TIMESTAMP, interval -7 day))
+  WHERE (buildtime > datetime(CURRENT_TIMESTAMP, '-7 days')) 
   GROUP BY user_id;
 
 COMMIT TRANSACTION;
