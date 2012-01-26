2460bb18e165fe1d4d7e5ba034106468ddccd469
Naohiro Aota <naota@elisp.net>
2012-01-27 02:51:11 +0900

    Translate to English

--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -13,25 +13,25 @@ class User < TwitterAuth::GenericUser
     mins = hours[1].divmod(60)
     
     if days[0] > 0
-      days[0].to_s + "日" + hours[0].to_s + "時間" + mins[0].to_s + "分"
+      days[0].to_s + "days" + hours[0].to_s + "h" + mins[0].to_s + "m"
     elsif hours[0] > 0
-      hours[0].to_s + "時間" + mins[0].to_s + "分"
+      hours[0].to_s + "h" + mins[0].to_s + "m"
     elsif mins[0] > 0
-      mins[0].to_s + "分" + mins[1].to_s + "秒"
+      mins[0].to_s + "m" + mins[1].to_s + "s"
     else
-      mins[1].to_s + "秒"
+      mins[1].to_s + "s"
     end      
   end
 
-  def toLimitedEmergesList(emerges, limit, initfoot, foot)
-    txt = emerges.collect{|e| e.package.fullname}.join(' ')+initfoot
+  def toLimitedEmergesList(emerges, limit, inithead, head)
+    txt = inithead+emerges.collect{|e| e.package.fullname}.join(' ')
     while txt.split(//u).length > limit
       if emerges.length == 1
-        txt = foot
+        txt = head
         break
       end
       emerges.pop
-      txt = emerges.collect{|e| e.package.fullname}.join(' ')+"など"+foot
+      txt = head+emerges.collect{|e| e.package.fullname}.join(' ')+" etc."
     end
     txt
   end
@@ -47,8 +47,8 @@ class User < TwitterAuth::GenericUser
 
     if emerges_failed.length > 0
       failed_txt = toLimitedEmergesList(emerges_failed, limit,
-                                        "のemergeに失敗しました。",
-                                        emerges_failed.length.to_s+"個のemergeに失敗しました。")
+                                        "Failed to emerge ",
+                                        "Failed to emerge "+emerges_failed.length.to_s+" packages ")
       limit -= failed_txt.split(//u).length
     else
       failed_txt = ""
@@ -57,9 +57,8 @@ class User < TwitterAuth::GenericUser
     if emerges_succ.length > 0
       succ_time = pretty_duration(emerges_succ.collect{|e| e.duration}.sum)
       succ_txt = toLimitedEmergesList(emerges_succ, limit,
-                                      "をemergeしました。(のべ"+succ_time+")",
-                                      emerges_succ.length.to_s +
-                                      "個をemergeしました。(のべ"+succ_time+")")
+                                      "emerge'd (in "+succ_time+")",
+                                      "emerge'd "+emerges_succ.length.to_s+" packages (in "+succ_time+") ")
     else
       succ_txt = ""
     end
@@ -68,9 +67,9 @@ class User < TwitterAuth::GenericUser
       emerges_succ = self.emerges.where("tobe_tweet = ? AND duration <> 0", true).order("buildtime desc")
       succ_time = pretty_duration(emerges_succ.collect{|e| e.duration}.sum)
       succ_txt = toLimitedEmergesList(emerges_succ, 140 - foot.length,
-                                      "をemergeしました。(のべ"+succ_time+")",
+                                      "emerge'd (in "+succ_time+")",
                                       emerges_succ.length.to_s +
-                                      "個をemergeしました。(のべ"+succ_time+")")
+                                      "emerge'd "+emerges_succ.length.to_s+" packages (in "+succ_time+") ")
       if failed_txt == ""
         [succ_txt+foot]
       else
